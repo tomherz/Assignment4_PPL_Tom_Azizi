ef6b162f4e5bd04bb1047e4c38f631a402bfdf9f
tomherz <tomherz@post.bgu.ac.il>
2026-06-24 11:20:57 +0300

    first commit

---
+++ ex4.pl
@@ -0,0 +1,40 @@
+/*
+ * **********************************************
+ * Printing result depth
+ *
+ * You can enlarge it, if needed.
+ * **********************************************
+ */
+maximum_printing_depth(100).
+
+:- current_prolog_flag(toplevel_print_options, A),
+   (select(max_depth(_), A, B), ! ; A = B),
+   maximum_printing_depth(MPD),
+   set_prolog_flag(toplevel_print_options, [max_depth(MPD)|B]).
+
+% Signature: sub_list(Sublist, List)/2
+% Purpose: All elements in Sublist appear in List in the same order.
+% Precondition: List is fully instantiated (queries do not include variables in their second argument).
+
+
+
+
+
+% Signature: swap_list(List, InversedList)/2
+% Purpose: InversedList is the ‘mirror’ representation of List, i.e, each item in the list is recursively replaced with the item at the position, with refers to the beginning and the end of the list.   
+
+
+
+
+
+
+
+
+% Signature: sub_tree(Subtree, Tree)/2
+% Purpose: Tree contains Subtree.
+
+
+
+
+% Signature: swap_tree(Tree, InversedTree)/2
+% Purpose: InversedTree is the �mirror� representation of Tree.
