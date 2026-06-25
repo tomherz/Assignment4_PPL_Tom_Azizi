364efe366214308a82b9b9c8b4a92e41a9920f42
Azizi976 <Tal.azizi123@gmail.com>
2026-06-25 12:21:42 +0300

    update prolog part

--- ex4.pl
+++ ex4.pl
@@ -23,7 +23,23 @@ sub_list(SubTail,[Head | Tail]) :- sub_list(SubTail,Tail). %if Subtail is a sub 
 % Signature: swap_list(List, InversedList)/2
 % Purpose: InversedList is the ‘mirror’ representation of List, i.e, each item in the list is recursively replaced with the item at the position, with refers to the beginning and the end of the list.   
 
+% Helper procedure to append two lists
+my_append([], L, L).
+my_append([H|T], L, [H|R]) :- my_append(T, L, R).
 
+% Case 1: Empty list
+swap_list([], []) :- !.
+
+% Case 2: The Head is itself a list.
+swap_list([[X|Y] | Tail], InversedList) :- !,
+    swap_list([X|Y], SwappedHead),
+    swap_list(Tail, SwappedTail),
+    my_append(SwappedTail, [SwappedHead], InversedList).
+
+% Case 3: The Head is a regular element.
+swap_list([Head|Tail], InversedList) :-
+    swap_list(Tail, SwappedTail),
+    my_append(SwappedTail, [Head], InversedList).
 
 
 
@@ -34,7 +50,23 @@ sub_list(SubTail,[Head | Tail]) :- sub_list(SubTail,Tail). %if Subtail is a sub 
 % Purpose: Tree contains Subtree.
 
 
+% Case 1: Search recursively in the Left subtree first
+sub_tree(Subtree, tree(_, Left, _)) :- sub_tree(Subtree, Left).
+
+% Case 2: Search recursively in the Right subtree next
+sub_tree(Subtree, tree(_, _, Right)) :- sub_tree(Subtree, Right).
+
+% Case 3: Match the current tree structure itself
+sub_tree(tree(X, Left, Right), tree(X, Left, Right)).
 
 
 % Signature: swap_tree(Tree, InversedTree)/2
 % Purpose: InversedTree is the �mirror� representation of Tree.
+
+% Case 1: Base case
+swap_tree(void,void):- !.
+
+% Case 2: Recursive case
+swap_tree(tree(Element, Left, Right), tree(Element, SwappedRight, SwappedLeft)) :- !,
+    swap_tree(Left, SwappedLeft),
+    swap_tree(Right, SwappedRight).
